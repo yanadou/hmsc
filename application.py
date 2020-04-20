3d8a459c92d83b37360550610fb76df9b564d71b
Bruce <1046244623@qq.com>
2020-04-20 17:27:23 +0800

    自定义指令，自定义服务器

--- application.py
+++ application.py
@@ -1,3 +1,11 @@
 from flask import Flask
+from flask_script import Manager
+import os
 
-app = Flask(__name__)
+class Applciation(Flask):
+    def __init__(self,import_name,template_folder=None,root_path=None):
+        super(Applciation, self).__init__(import_name,template_folder=template_folder,root_path=root_path)
+
+
+app = Applciation(__name__,template_folder=os.getcwd()+'/web/templates/',root_path=os.getcwd())
+manager = Manager(app)
