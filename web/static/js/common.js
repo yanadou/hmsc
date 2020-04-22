eb27d4f46e653bbe1ee2beec1390a37973f2b00c
Bruce <1046244623@qq.com>
2020-04-22 17:20:24 +0800

    登录模板，js，后端controller

--- web/static/js/common.js
+++ web/static/js/common.js
@@ -1,7 +1,15 @@
 ;
 var common_ops = {
     init:function(){
-        console.log('common.js初始化')
+        this.eventBind()
+    },
+    eventBind:function(){
+        console.log("common.js")
+    },
+    buildUrl:function(path,params){
+        var url = "" + path
+
+        return url;
     }
 }
 
